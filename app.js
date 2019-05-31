f3832d84507d418c31f61eb25799d3b0eaca1f23
botelja <ivo.surjak@gmail.com>
2019-05-31 13:25:58 +0200

    add task

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 //Storage Controller
-function Storage() {};
+function Storage(task) {
+   
+};
+
 
 //Task Controller
 function Task(task, completed = false) {
@@ -8,17 +11,58 @@ function Task(task, completed = false) {
 };
 
 //UI Controller
-function UI() {};
+function UI() {
+    this.UISelectors = {
+        addBtn: '.addBtn',
+        task: '.task',
+        tasks: '.tasks'
+    };
+
+    //Get input value
+    this.getTaskInput = () => {
+        return document.querySelector(this.UISelectors.task).value;
+    }
+};
+
+UI.prototype = {
+    populateTasks(task) {
+        let output = '';
+        output = `<li class="" id="task-1"><input type="checkbox" class="">${task.task}</li>`;
+
+        document.querySelector(this.UISelectors.tasks).innerHTML = output;
+    }
+}
 
 //App Controller
 function App() {
-    this.loadEventListeners = function() {
-        console.log('Event');
+    const ui = new UI();
+    this.loadEventListeners = () => {
+        //Add task listener
+        document.querySelector(ui.UISelectors.addBtn).addEventListener('click', addTasks);
+
+        //Add task
+    function addTasks() {
+        //Get task from input
+        const task = ui.getTaskInput();
+
+        const newTask = new Task(task);
+
+        //Check for task input
+        if(task !== ''){
+            //Add Task
+            ui.populateTasks(newTask);
+        }
     }
+    }
+
+    
 };
 
 App.prototype = {
     init() {
+
+      
+        //Load event listeners
         this.loadEventListeners();
     }
 };
